54017c27c0b41f82d329022325b2bc0f2c3e7b1a
Yair Castagnola <ycastagnola>
2025-03-26 18:16:10 -0300

    Agregado de clase Producto e interface del persistidor

---
+++ Admin_Productos/Admin_Productos/Persistidor/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin_Productos.Persistidor
+{
+    public class Producto
+    {
+        public int? Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Marca { get; set; }   
+        public double Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
